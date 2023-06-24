992b4b8799176797fa937f39b90efa45a2abc7fa
Xinqqqqqqqq <125946361+Xinqqqqqqqq@users.noreply.github.com>
2023-06-24 17:18:43 +0800

    Update DataSamling.py

--- DataSamling/DataSamling.py
+++ DataSamling/DataSamling.py
@@ -1,43 +1,16 @@
 import random
-import string
 
+def dataSampling(**kwargs):
+    result = []
+    for data_type, data_num in kwargs.items():
+        if data_type == "int":
+            result.extend(random.sample(range(1, 100), data_num))
+        elif data_type == "float":
+            result.extend([random.uniform(0, 1) for _ in range(data_num)])
+        elif data_type == "str":
+            result.extend([random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(data_num)])
+    return result
 
-def dataSampling():
-    """
-    :return:
-    """
-    datatype = input("请输入您想随机生成的类型：")
-    if datatype == "int" :
-        s = int(input("您想生成几位随机整数："))
-        random_int(s)
-    elif datatype == "float":
-        d = int(input("您想生成几位随机小数："))
-        random_float(d)
-    elif datatype == "str":
-        v = int(input("您想生成几位随机小数："))
-        random_str(v)
-    elif datatype == "tuple":
-        f = int(input("您想生成几位随机小数："))
-        random_tup(f)
-    else :
-        print("输入类型错误！")
-def random_int(num):
-    numbers = []
-    for i in range (num):
-        numbers.append(random.randint(0,10))
-    print(numbers)
-def random_float(num):
-    numbers = []
-    for i in range (num):
-        numbers.append(random.random(0,10))
-    print(numbers)
-def random_str(num):
-    random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=num))
-    print(random_string)
-def random_tup(num):
-    random_tuple = tuple(random.randint(1, 10) for _ in range(num))
-    print(random_tuple)
-
-
-dataSampling()
-
+# 调用示例
+# data = dataSampling(int=5, float=3, str=4)
+# print(data)
