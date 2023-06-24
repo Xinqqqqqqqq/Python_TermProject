ee7ad7c3a123de7f9e33856130985132124c8dd5
Xinqqqqqqqq <125946361+Xinqqqqqqqq@users.noreply.github.com>
2023-06-24 17:21:58 +0800

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -1,14 +1,13 @@
 import MLPackage.MLmodel as models
 import StandardPackage.standards as standards
-import DataSamling.DataSamling as DataSamling
+import DataSamling.DataSamling as dataSamling
 
-@DataSamling("int","float","str","tuple")
 @standards("MCC", "ACC")
 @models("SVM","RF","CNN","RNN")
 
 def SentificTest(*args,**kwargs):
     #1.Create Data
     pass
-    return DataSamling()
+    return dataSamling()
 
 SentificTest()
